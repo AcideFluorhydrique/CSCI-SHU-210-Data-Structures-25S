16985f53d003e8c4f4a296e87d5e41cd86bc2bdc
Ariel Xinyue Wang <139669368+AcideFluorhydrique@users.noreply.github.com>
2025-05-15 01:05:05 +0800

    Create ArrayQueue.py

---
+++ Code/ArrayQueue.py
@@ -0,0 +1,39 @@
+
+
+class ArrayQueue:
+    DEFAULT_CAPACITY = 10
+
+    def __init__(self):
+        self._data = [None] * ArrayQueue.DEFAULT_CAPACITY
+        self._size = 0
+        self._front = 0
+
+    def __len__(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def first(self):
+        if self.is_empty():
+            raise Exception("Queue is Empty")
+        return self._data[self._front]
+
+    def dequeue(self):
+        if self.is_empty():
+            raise Exception("Queue is Empty")
+        ans = self._data[self._front]
+        self._data[self._front] = None
+        self._front = (self._front + 1) % len(self._data)
+        self._size -= 1
+        return ans
+
+    def enqueue(self, e):
+        if self._size == len(self._data):
+            raise Exception("Queue is Full")
+        loc = (self._front + self._size) % len(self._data)
+        self._data[loc] = e
+        self._size += 1
+
+    def __str__(self):
+        return str(self._data)
