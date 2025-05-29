9855b539b4590401ce3b41274d42ea681ddd58c2
Ariel Xinyue Wang <139669368+AcideFluorhydrique@users.noreply.github.com>
2025-05-30 00:09:30 +0800

    Create DoubleLinkedList.py

---
+++ Code/DoubleLinkedList.py
@@ -0,0 +1,44 @@
+class DoubleLinkedList:
+    class Node:
+        def __init__(self, element = None, prev = None, next = None):
+            self._element = element
+            self._prev = prev
+            self._next = next
+
+    def __init__(self):
+        self._header = Node(None, None, None)
+        self._trailer = Node(None, None, None)
+        self._header._next = self._trailer
+        self._trailer._prev = self._header
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def _insert_between(self, e, predecessor, successor):
+        newest = Node(e, predecessor, successor)
+        predecessor._next = newest
+        successor._prev = newest
+        self._size += 1
+        return newest
+
+    def _delete_node(self, node):
+        predecessor = node._prev
+        successor = node._next
+        predecessor._next = successor
+        successor._prev = predecessor
+        self._size -= 1
+        element = node._element
+        node._prev = node._next = node._element = None
+        return element
+
+    def __str__(self):
+        result = "Header<-->"
+        currNode = self._header._next
+        while currNode is not self._trailer:
+            result += str(currNode._element) + "<-->"
+            currNode = currNode._next
+        return result + "Trailer"
