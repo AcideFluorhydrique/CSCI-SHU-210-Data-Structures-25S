9123f02e877ef56e791dc9e1d75bd43baf049841
Ariel Xinyue Wang <139669368+AcideFluorhydrique@users.noreply.github.com>
2025-05-31 00:10:18 +0800

    Update TreeWithParent.py

--- Code/TreeWithParent.py
+++ Code/TreeWithParent.py
@@ -1,3 +1,9 @@
+"""
+The TreeWithParent class is a simple representation of a node in a binary tree where 
+each node contains an element and may have parent, left, and right child nodes. 
+The class keeps a reference to a parent node, hence the name TreeWithParent.
+"""
+
 class TreeWithParent:
   def __init__(self, element, parent = None, left = None, right = None):
     self._element = element
