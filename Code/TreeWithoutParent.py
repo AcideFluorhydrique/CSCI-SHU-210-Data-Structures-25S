bd7a44856bd32a3c84d8fefa255a4df4a829cf26
Ariel Xinyue Wang <139669368+AcideFluorhydrique@users.noreply.github.com>
2025-06-01 18:47:33 +0800

    Create TreeWithoutParent.py

---
+++ Code/TreeWithoutParent.py
@@ -0,0 +1,8 @@
+class TreeWithoutParent:
+    def __init__(self, element, left = None, right = None):
+        self._element = element
+        self._left = left
+        self._right = right
+
+    def __str__(self):
+        return str(self._element)
