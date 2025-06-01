faa45edb3c7799f777044963136091973f4c4c1a
Ariel Xinyue Wang <139669368+AcideFluorhydrique@users.noreply.github.com>
2025-06-01 18:48:51 +0800

    Update TreeWithoutParent.py

--- Code/TreeWithoutParent.py
+++ Code/TreeWithoutParent.py
@@ -1,8 +1,8 @@
 """
-The TreeWithoutParent class is a simple representation of a node in a binary tree where each node contains an element and may have left and right child nodes. The class does not keep a reference to a parent node, hence the name TreeWithoutParent.
+The TreeWithoutParent class is a simple representation of a node in a 
+binary tree where each node contains an element and may have left and right child nodes. 
+The class does not keep a reference to a parent node, hence the name TreeWithoutParent.
 """
-
-
 
 class TreeWithoutParent:
     def __init__(self, element, left = None, right = None):
